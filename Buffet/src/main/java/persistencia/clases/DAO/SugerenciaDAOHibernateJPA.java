d600661c29113d9175268f9167a99e6b381a5492
Rafa <rafael.gomez.torres@hotmail.com>
2024-10-18 14:30:22 -0300

    SugerenciaDAOHibernateJPA implementada

---
+++ Buffet/src/main/java/persistencia/clases/DAO/SugerenciaDAOHibernateJPA.java
@@ -0,0 +1,69 @@
+package persistencia.clases.DAO;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import persistencia.clases.entidades.Sugerencia;
+import persistencia.interfaces.SugerenciaDAO;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.time.LocalDate;
+import java.util.List;
+
+public class SugerenciaDAOHibernateJPA extends GenericDAOHibernateJPA<Sugerencia> implements SugerenciaDAO {
+
+    public SugerenciaDAOHibernateJPA() {
+        super(Sugerencia.class);
+    }
+
+    private TypedQuery<Sugerencia> findByDateHelper(EntityManager em, LocalDate fecha) {
+        return em.createQuery("SELECT s FROM Sugerencia s WHERE s.fecha = :fecha ORDER BY s.fecha DESC", this.entityClass)
+                 .setParameter("fecha", fecha);
+    }
+
+    @Override
+    public List<Sugerencia> findByDate(LocalDate fecha) {
+        EntityManager em = EMF.getEMF().createEntityManager();
+        try {
+            return findByDateHelper(em, fecha).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Sugerencia> findByDate(LocalDate fecha, int maxResult) {
+        EntityManager em = EMF.getEMF().createEntityManager();
+        try {
+            return findByDateHelper(em, fecha)
+                   .setMaxResults(maxResult)
+                   .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Sugerencia> findAllOrderedByDateAsc() {
+        EntityManager em = EMF.getEMF().createEntityManager();
+        try {
+            return em.createQuery("SELECT s FROM Sugerencia s ORDER BY s.fecha ASC", this.entityClass)
+                     .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Sugerencia> findAllOrderedByDateDesc() {
+        EntityManager em = EMF.getEMF().createEntityManager();
+        try {
+            return em.createQuery("SELECT s FROM Sugerencia s ORDER BY s.fecha DESC", this.entityClass)
+                     .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+}
+
--- Buffet/src/main/java/persistencia/interfaces/SugerenciaDAO.java
+++ Buffet/src/main/java/persistencia/interfaces/SugerenciaDAO.java
@@ -10,6 +10,6 @@ public interface SugerenciaDAO extends GenericDAO<Sugerencia>{
 	/*metodo privado que retorna una query, uno usa esa query y el otro le agrega el limite*/
 	public List<Sugerencia> findByDate (LocalDate fecha);
 	public List<Sugerencia> findByDate (LocalDate fecha, int maxResult);
-	public List<Sugerencia> efindAllOrderedByDateAsc ();
-	public List<Sugerencia> efindAllOrderedByDateDesc ();
+	public List<Sugerencia> findAllOrderedByDateAsc ();
+	public List<Sugerencia> findAllOrderedByDateDesc ();
 }
